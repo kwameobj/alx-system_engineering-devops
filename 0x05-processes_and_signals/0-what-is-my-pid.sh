bce32c6e06351408a7384cabcbd60c241a6c662a
kwameobj <kowusubamfo@gmail.com>
2023-12-23 04:42:23 +0000

    Updated Task 0 file(s)

--- 0x05-processes_and_signals/0-what-is-my-pid.sh
+++ 0x05-processes_and_signals/0-what-is-my-pid.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
-# Displays its own pid
+# This Script displays it's own pid
 
 # echo "$$" | pgrep bash | awk '{print $1}'
-
 #OR
-
 # ps | pgrep bash | awk '{print $1}'
-
 # OR
-
 echo "$$"
